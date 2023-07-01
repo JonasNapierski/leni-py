b539b3ff100c1bea658d2c73ed7747dc46e703ea
Jonas Napierski <pudeljoni@gmail.com>
2023-07-01 12:44:28 +0200

    basic test setup

---
+++ tests/test_logging_command.py
@@ -0,0 +1,18 @@
+import logging
+from src.core.settings.logging import Configuration
+
+
+def test_empty_config():
+    log_conf = Configuration()
+
+    assert log_conf.default_level == logging.INFO
+    assert log_conf.logger_configugartion_path == "/app/logger/loggers.yml"
+    assert log_conf.output_directory == "/var/log/leni-py/"
+
+
+def test_command_loaded_config():
+    pass
+
+
+def test_environment_loaded_config():
+    pass
