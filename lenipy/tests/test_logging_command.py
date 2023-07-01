6082885ac85dd2452e8510a2fcafbf2f2787d235
Jonas Napierski <pudeljoni@gmail.com>
2023-07-01 13:10:14 +0200

    move leni inside project folder

--- lenipy/tests/test_logging_command.py
+++ lenipy/tests/test_logging_command.py
@@ -1,12 +1,12 @@
 import logging
-from Leni.src.core.settings.logging import Configuration
+from lenipy.src.core.settings.logging import Configuration
 
 
 def test_empty_config():
     log_conf = Configuration()
 
     assert log_conf.default_level == logging.INFO
-    assert log_conf.logger_configugartion_path == "/app/logger/loggers.yml"
+    assert log_conf.logger_configuration_path == "/app/logger/loggers.yml"
     assert log_conf.output_directory == "/var/log/leni-py/"
 
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name="leni-py",
+setup(name="leni_py",
       version="0.0.1",
       packages=find_packages()
       )
