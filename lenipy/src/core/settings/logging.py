5a784436539feec106e87ca3b1cfaf09b635ed63
Jonas Napierski <pudeljoni@gmail.com>
2023-07-01 16:10:59 +0200

    cannot load setting from environment variables

--- lenipy/src/core/settings/logging.py
+++ lenipy/src/core/settings/logging.py
@@ -1,10 +1,29 @@
 """static methode function & constants
 """
 import logging
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
+
+__config = None
+__command_arguments_parsed = False
 
 
 class Configuration(BaseSettings):
-    default_level: int = logging.INFO
-    logger_configuration_path: str = "/app/logger/loggers.yml"
-    output_directory: str = "/var/log/leni-py/"
+    default_level: int = Field(logging.INFO)
+    logger_configuration_path: str = Field("/app/logger/loggers.yml")
+    output_directory: str = Field("/var/log/leni-py/")
+
+
+def get_config() -> Configuration:
+    """
+
+    :rtype:
+    :return:
+    """
+    global __config
+
+    if __command_arguments_parsed:
+
+        return __config
+    else:
+        __config = Configuration()
+        return __config
--- lenipy/tests/test_logging_command.py
+++ lenipy/tests/test_logging_command.py
@@ -1,9 +1,10 @@
 import logging
-from lenipy.src.core.settings.logging import Configuration
+import os
+from lenipy.src.core.settings.logging import get_config
 
 
 def test_empty_config():
-    log_conf = Configuration()
+    log_conf = get_config()
 
     assert log_conf.default_level == logging.INFO
     assert log_conf.logger_configuration_path == "/app/logger/loggers.yml"
@@ -15,4 +16,12 @@ def test_command_loaded_config():
 
 
 def test_environment_loaded_config():
-    pass
+    os.environ["logging_defaul_level"] = "0"
+    os.environ["logging_logger_configuration_path"] = "/"
+    os.environ["logging_output_directory"] = "/"
+
+    log_conf = get_config()
+    log_conf._iter
+    assert log_conf.default_level == 0
+    assert log_conf.logger_configuration_path == "/"
+    assert log_conf.output_directory == "/"
