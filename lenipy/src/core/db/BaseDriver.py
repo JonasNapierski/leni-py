6449ac1be9c7ae2de08a934c30c473f6c7cc8b20
Jonas Napierski <pudeljoni@gmail.com>
2023-07-04 18:49:02 +0200

    add getter for lists and missing getters

--- lenipy/src/core/db/BaseDriver.py
+++ lenipy/src/core/db/BaseDriver.py
@@ -15,20 +15,35 @@ class BaseDriver:
     def create_modules(module: Modules):
         pass
 
-    def get_modules(mod_id: str) -> Modules:
+    def get_modules_list():
+        pass
+
+    def get_modules_by_id(mod_id: str) -> Modules:
         pass
 
     def create_mod_config(mod_config: ModuleConfig):
         pass
 
-    def get_mod_config(mod_id: str, mod_field: str) -> ModuleConfig:
+    def get_mod_config_list():
+        pass
+
+    def get_mod_config_by_field(mod_id: str, mod_field: str) -> ModuleConfig:
         pass
 
     def create_mod_command(mod_command: ModuleCommand):
         pass
 
-    def get_mod_command(mod_id: str, command: str):
+    def get_mod_command_list():
+        pass
+
+    def get_mod_command_by_command(mod_id: str, command: str):
         pass
 
     def create_command_example(cmd_example: CommandExample):
+        pass
+
+    def get_command_example_list():
+        pass
+
+    def get_command_example_by_example(example: str):
         pass
