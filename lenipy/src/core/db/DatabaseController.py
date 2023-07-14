d9bb0a69358f14bc8e40ce640fe1312a5a46b890
Jonas Napierski <pudeljoni@gmail.com>
2023-07-14 08:33:17 +0200

    add getting selected driver for future use

--- lenipy/src/core/db/DatabaseController.py
+++ lenipy/src/core/db/DatabaseController.py
@@ -1,7 +1,11 @@
+import os 
+import logging
 from BaseDriver import BaseDriver
 from driver.Pocketbase import PocketbaseDriver
 from lenipy.src.core.modules.Module import Module
 from lenipy.src.core.models.response import ResponseCode
+
+log = logging.getLogger("leni.db.controller")
 
 
 class DatabaseController:
@@ -20,3 +24,33 @@ class DatabaseController:
         """
 
         return ResponseCode(code=200)
+
+    def __get_selected_db_driver(self) -> BaseDriver or None:
+        """Tries to initilize the Driver, that is selected by the env variable
+        $DB_DRIVER.
+        :return: Returns a child of the BaseDriver
+        :rtype: BaseDriver or None
+        """
+        env_driver = os.getenv("DB_DRIVER")
+
+        if env_driver is None:
+            return
+
+        if env_driver == "PocketBase":
+            env_username = os.getenv("DB_USERNAME")
+            env_password = os.getenv("DB_PASSWORD")
+            env_url = os.getenv("DB_PB_URL")
+
+            if env_url is None or \
+                    env_password is None or \
+                    env_username is None:
+                log.info("You selected the Pocketbase database. Please make\
+                        sure to the all the necessary")
+                log.info("DB_USERNAME, DB_PASSWORD and DB_PB_URL to establish\
+                        a connection to the Database.")
+            driver_pb = PocketbaseDriver(user=env_username,
+                                         password=env_password,
+                                         url=env_url)
+            if driver_pb is None:
+                log.warn("Something went wrong during the Connection Process.")
+            return driver_pb
